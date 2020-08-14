3382109ec768e38176d5bbcd741b819e65c76629
Jan Gradič <gradijan957@gmail.com>
2020-08-14 19:49:02 +0200

    fixed event

--- lambdas/main/index.py
+++ lambdas/main/index.py
@@ -3,10 +3,6 @@ import datetime
 
 
 def handler(event, context):
-    data = {
-        'event': json.dumps(event),
-        'context': json.dumps(context)
-    }
     return {'statusCode': 200,
-            'body': json.dumps(data),
+            'body': json.dumps(event),
             'headers': {'Content-Type': 'application/json'}}
