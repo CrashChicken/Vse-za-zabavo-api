ba05f4fbfe7bb5feef2f4addd745ecd83d5ea152
Jan Gradič <gradijan957@gmail.com>
2020-08-15 00:23:19 +0200

    added test path

--- lambdas/prostori/index.py
+++ lambdas/prostori/index.py
@@ -3,10 +3,11 @@ import datetime
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
+    if event.httpMethod == "GET":
+        data = {
+            'output': 'GET',
+            'timestamp': datetime.datetime.utcnow().isoformat()
+        }
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
