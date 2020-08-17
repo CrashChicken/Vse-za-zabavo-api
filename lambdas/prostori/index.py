2f7dcb991aebd53f093df53224d3edd09f4d327c
Jan Gradič <jan@gradic.net>
2020-08-17 02:24:56 +0200

    added delete function

--- lambdas/prostori/index.py
+++ lambdas/prostori/index.py
@@ -2,38 +2,67 @@ import json
 import boto3
 import os
 import datetime
+import uuid
 
 tableName = os.environ['tableName']
 dynamodb = boto3.resource('dynamodb')
+#tableName = "prostori"
+#dynamodb = boto3.resource('dynamodb', endpoint_url="http://172.18.0.2:8000")
 table = dynamodb.Table(tableName)
 
 def handler(event, context):
+    
     if event['httpMethod'] == "GET":
-        response = table.scan()
-        items = response['Items']
-        return {'statusCode': 200,
-            'body': json.dumps(items),
-            'headers': {'Content-Type': 'application/json'}}
+        try:
+            response = table.scan()
+            items = {'data': response['Items']}
+        except Exception as e:
+            message = {'error': str(e)}
+            return http_response(message, 500)
+        else:
+            return http_response(items, 200)
+
     elif event['httpMethod'] == "POST":
         try:
             submittedItems = json.loads(event['body'])
         except:
             message = {'error': 'JSON ni pravilno oblikovan'}
-            return {
-                'statusCode': 400,
-                'body': json.dumps(message),
-                'headers': {'Content-Type': 'application/json'}
-            }
-        table.put_item(
-            Item={
-                'id_prostora': submittedItems['id_prostora'],
-                'uporabnik': event['requestContext']['authorizer']['claims']['cognito:username'],
-                'ime': submittedItems['ime'],
-                'opis': submittedItems['opis'],
-                'regija': submittedItems['regija'],
-            }
-        )
-        message = {'id_prostora': submittedItems['id_prostora']}
-        return {'statusCode': 201,
-                'body': json.dumps(message),
-                'headers': {'Content-Type': 'application/json'}}
+            return http_response(message, 400)
+        else:
+            id_prostora = str(uuid.uuid4())
+            try:
+                table.put_item(
+                    Item={
+                        'id_prostora': id_prostora,
+                        'id_uporabnika': event['requestContext']['authorizer']['claims']['cognito:username'],
+                        'ime': submittedItems['ime'],
+                        'opis': submittedItems['opis'],
+                        'regija': submittedItems['regija'],
+                        'ustvarjeno': datetime.datetime.now()
+                    }
+                )
+            except:
+                message = {'error': 'Podatki niso bili shranjeni'}
+                return http_response(message, 500)
+            else:
+                message = {'id_prostora': id_prostora}
+                return http_response(message, 201)
+
+    elif event['httpMethod'] == "DELETE":
+        try:
+            response = table.delete_item(
+                Key={
+                    'id_prostora': event['pathParameters']['id_prostora'],
+                    'id_uporabnika': event['requestContext']['authorizer']['claims']['cognito:username']
+                }
+            )
+        except ClientError as e:
+            return http_response(e, 400)
+        else:
+            return http_response(response, 200)
+def http_response(body, code):
+    return {
+        'statusCode': code,
+        'body': json.dumps(body),
+        'headers': {'Content-Type': 'application/json'}
+    }
